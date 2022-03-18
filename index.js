80865411f0de747915b21d85cb00c700f33fe94e
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-03-19 00:25:41 +0100

    Server connected to dataBase

--- index.js
+++ index.js
@@ -1,5 +1,18 @@
 const express = require("express");
 const app = express();
+const mongoDB = require("dotenv");
+const mongoose = require("mongoose");
+
+mongoDB.config();
+
+mongoose
+  .connect(process.env.MONGO_DB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(console.log("DataBase Connected Succesfully"))
+  .catch((err) => console.log(err));
+
 app.listen("3000", () => {
   console.log("Wakeing Up the server B0$$");
 });
