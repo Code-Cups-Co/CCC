ba2589b6a09182a3d997614d73bcc72c7badb3b0
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-03-19 10:52:19 +0100

    .env file hidden

--- routes/auth.js
+++ routes/auth.js
@@ -20,13 +20,15 @@ router.post("/register", async (req, res) => {
 
 router.post("/login", async (req, res) => {
   try {
-    const user = await User.findOne({ userName: req.body.newUser });
+    const user = await User.findOne({ userName: req.body.userName });
+    console.log(user);
     !user && res.status(400).json("Bad credentials");
 
     const validated = await bcrypt.compare(res.body.password, user.password);
     !validated && res.status(400).json("Bad credentials");
 
     res.status(200).json(user);
+    console.log(res.status(200).json(user));
   } catch (err) {
     res.status(500).json(err);
   }
