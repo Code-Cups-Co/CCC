c2bbdf5dacc1290c41450f9cba18d94f3e1f2c6e
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-03-19 09:32:48 +0100

    Added auth to newUser and loginUser

--- index.js
+++ index.js
@@ -2,8 +2,10 @@ const express = require("express");
 const app = express();
 const mongoDB = require("dotenv");
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
 
 mongoDB.config();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO_DB_URL, {
@@ -12,6 +14,8 @@ mongoose
   })
   .then(console.log("DataBase Connected Succesfully"))
   .catch((err) => console.log(err));
+
+app.use("/auth", authRoute);
 
 app.listen("3000", () => {
   console.log("Wakeing Up the server B0$$");
---
+++ routes/auth.js
@@ -0,0 +1,35 @@
+const router = require("express").Router();
+const bcrypt = require("bcrypt");
+const User = require("../models/User");
+
+router.post("/register", async (req, res) => {
+  try {
+    const salt = await bcrypt.genSalt(12);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+    const newUser = new User({
+      userName: req.body.userName,
+      email: req.body.email,
+      password: hashedPass,
+    });
+    const user = await newUser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.post("/login", async (req, res) => {
+  try {
+    const user = await User.findOne({ userName: req.body.newUser });
+    !user && res.status(400).json("Bad credentials");
+
+    const validated = await bcrypt.compare(res.body.password, user.password);
+    !validated && res.status(400).json("Bad credentials");
+
+    res.status(200).json(user);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
