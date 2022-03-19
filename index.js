a1ca842b5d2617fd052313d91f0b8619f9fb6b97
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-03-19 16:50:00 +0100

    Image Upload router finished

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
 const postRoute = require("./routes/posts");
 const categoryRoute = require("./routes/categorys");
+const multer = require("multer");
 
 mongoDB.config();
 app.use(express.json());
@@ -17,6 +18,20 @@ mongoose
   })
   .then(console.log("DataBase Connected Succesfully"))
   .catch((err) => console.log(err));
+
+const storage = multer.diskStorage({
+  destination: (req, fule, cb) => {
+    cb(null, "Images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, "icon2.png");
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/upload", upload.single("file"), (req, res) => {
+  res.status(200).json("Image has been uploaded");
+});
 
 app.use("/auth", authRoute);
 app.use("/users", userRoute);
