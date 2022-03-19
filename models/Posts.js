ad95b98adbacc91b598974d3c74e97b5d6f9af45
Joel Deon Dsouza <oka.welt.dev@gmail.com>
2022-03-19 08:14:00 +0100

    Models added with requirements

---
+++ models/Posts.js
@@ -0,0 +1,30 @@
+const mongoose = require("mongoose");
+
+const PostsSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    photo: {
+      type: String,
+      required: false,
+    },
+    userName: {
+      type: String,
+      required: true,
+    },
+    categorys: {
+      type: Array,
+      required: false,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Post", PostsSchema);
